4c9bb0b0a133122cf663c429ac8fadb51f357099
amirrezaDev1378 <rezaamir731@gmail.com>
2022-06-21 14:48:41 +0430

    initialized the project

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,22 +1,45 @@
 import {NextComponentType} from "next";
 
-import React from 'react';
-import {Stack, Typography} from "@mui/material";
-import {menuIcon , userIcon} from "../../assets/";
+import React, {AnchorHTMLAttributes, useRef} from 'react';
+import {Button, Stack, Typography} from "@mui/material";
+import {menuIcon, userIcon} from "../../assets/";
 import styles from "./header.module.scss";
 
+import {Analytics, AnalyticsOutlined, AutoGraphOutlined, Close, DashboardCustomize, NotificationsActiveOutlined} from "@mui/icons-material";
+
+const menuClickHandler = (e: React.MouseEvent<HTMLAnchorElement | SVGSVGElement>, menuRef: React.RefObject<HTMLElement>) => {
+    e.preventDefault();
+    if (menuRef.current.classList.contains(styles.menuBoxHidden)) {
+        menuRef.current.classList.remove(styles.menuBoxHidden);
+        menuRef.current.classList.add(styles.menuBoxVisible);
+
+    } else {
+        menuRef.current.classList.add(styles.menuBoxHidden);
+        menuRef.current.classList.remove(styles.menuBoxVisible);
+
+    }
+}
+
+const dashboardLinkSwitch = (): void => {
+    alert("Sorry but this feature is not yet available 😔");
+}
+
 const Header: NextComponentType = () => {
+    const menuBoxRef = useRef(null)
     return (
         <>
             <Stack alignItems={"center"} justifyContent={"space-around"} className={styles.headerMain} direction={'row'}>
                 <div className="">
-                    <a>
+                    <a
+                        onClick={(e) => menuClickHandler(e, menuBoxRef)}
+                        className={styles.menu}
+                    >
                         <img src={menuIcon.src} alt=""/>
                     </a>
                 </div>
                 <Typography variant={"h4"}>
                     Dashboard
-                    <Typography variant={"body1"}>
+                    <Typography variant={"body1"} color={"secondary.main"}>
                         Welcome back, Andrew
                     </Typography>
                 </Typography>
@@ -26,6 +49,28 @@ const Header: NextComponentType = () => {
                     </a>
                 </div>
             </Stack>
+
+            <div ref={menuBoxRef} className={`${styles.menuBox} ${styles.menuBoxHidden}`}>
+                <Close onClick={(e) => menuClickHandler(e, menuBoxRef)} color={"warning"} className={styles.menuCloseButton}/>
+
+                <Button onClick={dashboardLinkSwitch} className={styles.menuButtons} variant={"outlined"} color={"primary"}>
+                    Dashboard <DashboardCustomize/>
+                </Button>
+
+
+                <Button onClick={dashboardLinkSwitch} className={styles.menuButtons} variant={"outlined"} color={"primary"}>
+                    Analytics <AnalyticsOutlined/>
+                </Button>
+
+                <Button onClick={dashboardLinkSwitch} className={styles.menuButtons} variant={"outlined"} color={"primary"}>
+                    Trading <AutoGraphOutlined/>
+                </Button>
+
+                <Button onClick={dashboardLinkSwitch} className={styles.menuButtons} variant={"outlined"} color={"primary"}>
+                    Notifications <NotificationsActiveOutlined/>
+                </Button>
+
+            </div>
         </>
     );
 }
--- src/theme/defaultTheme.ts
+++ src/theme/defaultTheme.ts
@@ -4,14 +4,23 @@ import {responsiveFontSizes} from "@mui/material";
 const defaultTheme = createTheme({
     typography: {
         body1: {
-            fontSize: "1rem",
+            fontSize: "0.8rem",
         },
+        h5:{
+            fontWeight:"100"
+        },
+        h3:{
+            fontWeight:"150"
+        }
 
     },
     palette: {
         success: {
             main: "#00E38C",
         },
+        secondary:{
+            main:"#8990AD"
+        }
     },
 });
 
